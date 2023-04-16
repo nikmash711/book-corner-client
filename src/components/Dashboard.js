8b63f3da1765011a9914e619384207c431e52f1f
Nikkie Mashian <nikkiemashian@gmail.com>
2023-04-16 11:24:11 -0700

    add back

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -297,17 +297,6 @@ export default function Dashboard(props) {
         <Navbar />
         <main className="dashboard">
           {category && <h1 className="page-title">{titleKey[category]}</h1>}
-          {
-            <h2
-              style={{
-                color: 'red',
-                padding: '0px 40px 20px 40px',
-                textAlign: 'center',
-              }}
-            >
-              PLEASE NOTE: Library will be closed from April 3 - April 16!
-            </h2>
-          }
           <section className="user-directory">
             <input
               type="search"
@@ -360,17 +349,6 @@ export default function Dashboard(props) {
         <Navbar />
         <main className="dashboard">
           {category && <h1 className="page-title">{titleKey[category]}</h1>}
-          {
-            <h2
-              style={{
-                color: 'red',
-                padding: '0px 40px 20px 40px',
-                textAlign: 'center',
-              }}
-            >
-              PLEASE NOTE: Library will be closed from April 3 - April 16!
-            </h2>
-          }
           {admin && category === 'allCheckedOutMedia' && (
             <button
               className="action-button-skin media-button send-reminders-btn"
